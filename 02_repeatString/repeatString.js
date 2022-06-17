c26e7ac77fb6bc002f0ae05675280cdc19eb16aa
Brennan McKnight <skyosto321@gmail.com>
2022-06-17 13:01:44 -0500

    Create solution for 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(string, count) {
+    if (count < 0) return 'ERROR';
 
+    let stringResult = '';
+    for (let counter = 0; counter < count; counter++) {
+        stringResult += string
+    }
+    return stringResult;
 };
 
 // Do not edit below this line
