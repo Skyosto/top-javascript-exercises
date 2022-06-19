3f33938342a5078a1844ef00313c9997beea3ae4
Brennan McKnight <skyosto321@gmail.com>
2022-06-19 17:36:20 -0500

    Create solution for 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    // Count down from end of string to new string.
+    reversedString = '';
+    for (let i = (string.length - 1); i >= 0; i--) {
+        reversedString += string[i];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
