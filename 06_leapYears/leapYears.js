b13040451830e3ba125ce334b6b3cb74f8bf22f9
Brennan McKnight <skyosto321@gmail.com>
2022-06-20 14:09:21 -0500

    Create solution for 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(int) {
+    if (int % 4 === 0 ) {
+        if(int % 400 === 0 && int % 100 === 0) {
+            return true;
+        }
+        if (int % 400 !== 0 && int % 100 === 0) {
+            return false;
+        } 
+        else return true;
+    }
+    else return false;
 };
 
 // Do not edit below this line
