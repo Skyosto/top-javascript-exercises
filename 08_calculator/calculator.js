3413230eb30c79fd3cce233b8ab7bb2368f5dbcd
Brennan McKnight <skyosto321@gmail.com>
2022-07-06 10:27:21 -0500

    Create solution for 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numberArray) {
+	let sum = 0;
+  if (numberArray.length <= 0) return sum;
+  for (number of numberArray) {
+    sum += number;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numberArray) {
+  let product = numberArray[0];
+  if (numberArray.length <= 0) return product;
+  for (let i = 1; i < numberArray.length; i++) {
+    product *= numberArray[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num === 1) return 1;
+	let product = num;
+  num -= 1;
+  // Take the number, and multiply by itself - 1
+  do {
+    product = multiply([product, num]);
+    num--;
+  } while ( num > 1)
+  return product;
 };
 
 // Do not edit below this line
