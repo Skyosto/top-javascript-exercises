3f428f27e711803f0a9707fc644793e210f7d944
Brennan McKnight <skyosto321@gmail.com>
2022-07-06 11:59:25 -0500

    Create solution for 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(count) {
+    // Looks like we need to build the fibonacci while keeping the index.
+    // Also this fibonacci is going to be offset by 1 (doesn't start at 0, so to speak)
+    if (count < 0) return 'OOPS';
+    if (typeof count === 'string') count = parseInt(count);
+    // Build fibonacci (as array) to count
+    let fibonacci = [1, 1]
+    for (let i = 1; i < count + 1; i++) {
+        fibonacci.push(fibonacci[i - 1] + fibonacci[i]);
+    }
+    // Grab number at index of count
+    return fibonacci[count - 1];
 };
 
 // Do not edit below this line
