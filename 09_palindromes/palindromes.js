b641f6c9d9b1178c6f204ed19da692eae2844306
Brennan McKnight <skyosto321@gmail.com>
2022-07-06 11:19:03 -0500

    Create solution for 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+// Get a string of only letters
+let characters = string.match(/[a-zA-Z]/g).join("").toLowerCase();
+// Split to 2 strings in half (rounding up)
+let half1 = characters.substring(0, Math.round(characters.length / 2));
+let half2 = characters.substring(Math.floor(characters.length / 2), characters.length).split("").reverse().join("");
+// If strings ==, palindrome
+return half1 == half2 ? true : false;
 };
 
 // Do not edit below this line
